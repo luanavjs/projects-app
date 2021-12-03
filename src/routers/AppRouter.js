1730c0f530a337efb7cc5f57f724c2bcdbe99535
Luana <luanalorenavallejos@gmail.com>
2021-12-03 16:26:42 -0300

    agrego layout

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -7,23 +7,26 @@ import ProjectPage from '../pages/ProjectPage'
 import NotFoundPage from '../pages/NotFoundPage'
 import UsersPage from '../pages/admin/UsersPage'
 import AccountPage from '../pages/AccountPage'
+import Layout from '../components/layouts/Layout'
 
 
 export default function AppRouter() {
     return (
             <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<HomePage/>}/>
-                    <Route path="/login" element={<LoginPage/>}/>
-                    <Route path="/register" element={<RegisterPage/>}/>
-                    <Route path="/account" element={<AccountPage/>}/>
-                    <Route path="/projects" element={<ProjectsPage/>}/>
-                    <Route path="/project/:projectId" element={<ProjectPage/>}/>
-                    <Route path="/admin/users" element={<UsersPage/>}/>
+                <Layout>         
+                    <Routes>
+                        <Route path="/" element={<HomePage/>}/>
+                        <Route path="/login" element={<LoginPage/>}/>
+                        <Route path="/register" element={<RegisterPage/>}/>
+                        <Route path="/account" element={<AccountPage/>}/>
+                        <Route path="/projects" element={<ProjectsPage/>}/>
+                        <Route path="/project/:projectId" element={<ProjectPage/>}/>
+                        <Route path="/admin/users" element={<UsersPage/>}/>
 
-                    <Route path="*" element={NotFoundPage}/>
+                        <Route path="*" element={<NotFoundPage/>}/>
 
-                </Routes>
+                    </Routes>
+                </Layout>
             </BrowserRouter>
     )
 }
